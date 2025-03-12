c4f543d9ed46d7c8dd19e01143541abbf54bdb76
Qpicpicxxz <qpicpicxxz@outlook.com>
2025-03-12 14:09:05 +0800

    Guessing game finish

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+关键字Keywords
+- `const` 常量/不变裸指针(直接使用内存地址的指针)
+- `let` 绑定一个变量
+- `match` 模式匹配
+- `pub` 表示结构体字段
+
+Crate是Rust编译器处理的基本编译单元, 分为Binary Crate和Library Crate(有点类似c的.so/.ao?)
+
+更新完Cargo.toml之后`cargo build`会从registry上获取所有包的最新版本信息
+
+语义化版本(semantic versioning, SemVer)是一种标准化的版本号格式
+ - MAJOR.MINOR.PATCH
+ - MAJOR -> 不兼容的API变更
+ - MINOR -> 向后兼容的功能新增
+ - PATCH -> 向后兼容的问题修正
+
 Cargo.lock - 记录项目依赖的实际版本，永远别碰这个文件就好了
 
 TOML格式(Tom's Obvious, Minimal Language)
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,11 @@
 // use关键字: 将某类型显式地将其引入作用域
 // Rust设定了一些会自动导入到每个程序作用域中的标准库内容(预导入prelude), 例如println
 use std::io;
+// 在Rust中, trait定义的方法默认不在作用域中(Rust设计哲学 - 避免隐式的行为)
+// trait定义了一组方法签名
+use rand::Rng;
+// Ordering也是一个枚举, 成员是Less/Greater/Equal
+use std::cmp::Ordering;
 
 // fn关键字: 声明一个新函数
 fn main() {
@@ -10,17 +15,52 @@ fn main() {
     // let apples = 5 -> Rust中，变量默认是不可变的, 添加mut关键字让变量可变
     // String是UTF-8编码的可增长文本块
     // ::表明new是String类型的一个关联函数associated function(针对某个类型实现的函数)
-    let mut guess = String::new();
+    // let mut guess = String::new();
+
     // 如果开头没有引入io库, 则可以std::io::stdin
     // stdin函数返回一个std::io::Stdin的实例，代表终端标准输入句柄的类型
-    // &guess默认是不可变的, 因此需要&mut guess
+    // &guess默认是不可变的(immutable), 因此需要&mut guess
     // 调用了句柄上的read_line方法(无论输入什么都将其追加到字符串中)
     // 返回一个枚举类型Result(enum) -> Ok/Err
     // Result实例拥有expect方法, 如果不调用expect程序编译会出现警告
-    io::stdin()
+    // io::stdin()
+    //     .read_line(&mut guess)
+    //     .expect("Failed to read line");
+
+    // {}是个占位符, 用于格式化字符串里面的参数传入
+    // println!("You guessed: {}", guess);
+
+    // Rust允许用一个新值来隐藏shadowing之前的值, 常用语需要转换值类型的场景
+    // String 实例的 trim 方法会去除字符串开头和结尾的空白字符
+    // guess 后面的冒号（:）告诉 Rust 我们指定了变量的类型
+    // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    // secret_number是一个数字类型
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    // {}是个占位符, 用于格式化字符串里面的参数传入
-    println!("You guessed: {}", guess);
+        // guess.trim()有点类似pub fn trim(&self) -> &str
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
